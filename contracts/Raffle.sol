c2df5e6d61f5e6563745d88539c164b01ce2dc0e
Rathish <rathishvbr@gmail.com>
2025-05-01 13:06:00 +0200

    Compiled stage [All working fine]

---
+++ contracts/Raffle.sol
@@ -0,0 +1,16 @@
+// Raffle
+
+// Enter the lottery (paying some amount)
+// Pick a random winner (verifiably random)
+// Winner to be selected every X minutes -> completely random
+
+
+// Chainlink Oracle -> Randomness, Automated Execution (Chainlink Keepers)
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Raffle {
+
+}
+
