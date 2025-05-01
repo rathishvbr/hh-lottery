575b97035c7778d702a800e730831b661b7fef43
Rathish <rathishvbr@gmail.com>
2025-05-01 21:22:14 +0200

    Base functions

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -10,7 +10,36 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract Raffle {
+error Raffle__NotEnoughEthEntered();
 
+contract Raffle {
+    // State Variables
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players; // Storage array
+
+    // Events
+    event RaffleEnter(address indexed player);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthEntered();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+    
+    // function pickRandomWinner() {}
+
+    function getEntranceFee() public view returns(uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns(address) {
+        return s_players[index];
+    }
 }
 
