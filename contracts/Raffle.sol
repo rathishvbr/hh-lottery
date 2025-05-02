0ccf768822d84f50a7e2aadfb11ec06cc423b9c2
Rathish <rathishvbr@gmail.com>
2025-05-02 21:18:19 +0200

    Added chainlink VRF and upkeep

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -4,42 +4,148 @@
 // Pick a random winner (verifiably random)
 // Winner to be selected every X minutes -> completely random
 
-
 // Chainlink Oracle -> Randomness, Automated Execution (Chainlink Keepers)
 
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-error Raffle__NotEnoughEthEntered();
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
-contract Raffle {
+/**
+ * @title Raffle
+ * @author Rathish Vijayakumar
+ * @notice This contract is a simple lottery contract that allows users to enter and win a prize.
+ * @dev This contract uses Chainlink VRFv2.5 to generate a random winner.
+ */
+contract Raffle is VRFConsumerBaseV2Plus {
+    // Errors
+    error Raffle__NotEnoughEthEntered();
+    error Raffle__TransferFailed();
+    error Raffle__RaffleNotOpen();
+    error Raffle__UpkeepNotNeeded(uint256 balance, uint256 playersLength, uint256 raffleState);
+    // Type Declarations
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
     // State Variables
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    bytes32 private immutable i_keyHash;
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint256 private s_lastTimeStamp;
     address payable[] private s_players; // Storage array
+    address payable private s_recentWinner;
+    RaffleState private s_raffleState;
 
     // Events
     event RaffleEnter(address indexed player);
-
-    constructor(uint256 entranceFee) {
+    event WinnerPicked(address indexed winner);
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 keyHash,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_keyHash = keyHash;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
     }
 
     function enterRaffle() public payable {
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__RaffleNotOpen();
+        }
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEthEntered();
         }
         s_players.push(payable(msg.sender));
         emit RaffleEnter(msg.sender);
     }
-    
-    // function pickRandomWinner() {}
 
-    function getEntranceFee() public view returns(uint256) {
+    /**
+     * @dev This is the function that is called by the Chainlink nodes will call to see
+     * if the lottery is ready to be winner picked.
+     * The following should be true for this function to return true:
+     * 1. The lottery is open.
+     * 2. The lottery has enough players.
+     * 3. The lottery has enough time passed since the last winner was picked.
+     * 4. Implicitly, the contract has enough LINK.
+     * @param - ignore
+     * @return upkeepNeeded - true if the lottery is ready to be winner picked, false otherwise.
+     * @return performData - ignore
+     */
+    function checkUpkeep(bytes memory /* checkData */) public view returns (bool upkeepNeeded, bytes memory /* performData */) {
+        bool timeHasPassed = (block.timestamp - s_lastTimeStamp) >= i_interval;
+        bool isOpen = s_raffleState == RaffleState.OPEN;
+        bool hasPlayers = s_players.length > 0;
+        bool isBalanceEnough = address(this).balance > 0;
+        upkeepNeeded = (timeHasPassed && isOpen && hasPlayers && isBalanceEnough);
+        return (upkeepNeeded, "");
+    }
+
+    /**
+     * @dev This is the function that is called by the Chainlink nodes will call to pick a winner.
+     * @param - ignore
+     */
+    function performUpkeep(bytes calldata /* performData */) external {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Raffle__UpkeepNotNeeded(address(this).balance, s_players.length, uint256(s_raffleState));
+        }
+        s_raffleState = RaffleState.CALCULATING;
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATIONS,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+            )
+        });
+        s_vrfCoordinator.requestRandomWords(request);
+    }
+
+    function fulfillRandomWords(
+        uint256 /* requestId */,
+        uint256[] calldata randomWords
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        s_raffleState = RaffleState.OPEN;
+        // Reset the players array
+        s_players = new address payable[](0);
+        // Reset the last time stamp
+        s_lastTimeStamp = block.timestamp;
+        // Transfer the balance to the winner
+        emit WinnerPicked(recentWinner);
+
+
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+    }
+
+    function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
 
-    function getPlayer(uint256 index) public view returns(address) {
+    function getPlayer(uint256 index) public view returns (address) {
         return s_players[index];
     }
 }
-
